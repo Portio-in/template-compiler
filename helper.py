fdd397504759e1ab305bacdc28ff9124a1250b7f
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-02-14 00:27:56 +0530

    resume template issues fixed and education added

--- helper.py
+++ helper.py
@@ -4,6 +4,6 @@ def datetimeStringTodate(dateString):
     if dateString is None or dateString ==  "":
         return "Present"
     try:
-        return datetime.strptime("2023-01-19T18:30:00.000Z", "%Y-%m-%dT%H:%M:%S.%fZ").strftime('%d-%m-%Y')
+        return datetime.strptime(dateString, "%Y-%m-%dT%H:%M:%S.%fZ").strftime('%d-%m-%Y')
     except:
         return "Present"
